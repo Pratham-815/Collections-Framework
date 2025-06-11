da6ca9a8d95e811aadd21619f02ce59dfc9b0bb8
Pratham-815 <prathamagarwal815@gmail.com>
2025-06-11 11:41:08 +0530

    Refactor Main.java to initialize ArrayList with a capacity and add commented examples for list operations

--- src/List/Main.java
+++ src/List/Main.java
@@ -4,6 +4,43 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
+
+        List<Integer> list = new ArrayList<>(11);
+//        list.add(10);
+//        list.add(20);
+//        list.add(30);
+//        list.remove(Integer.valueOf(10));   // remove an element by value in a list
+//
+
+//        for(int i=0; i<list.size(); i++){
+//            System.out.println(list.get(i));
+//        }
+
+
+//        for(int x: list){
+//            System.out.println(x);
+//        }
+
+
+//        System.out.println(list.contains(20));
+//        System.out.println(list.contains(25));
+
+
+//        list.add(2, 25);           // add 25 at index 2 and shift 30 to index 3
+
+
+//        list.set(2, 25);        // replace element at index 2 with 25
+
+
+//        List<String> list1= Arrays.asList("Monday", "Tuesday", "Wednesday");
+//        System.out.println(list1.getClass().getName());        // java.util.Arrays$ArrayList
+//
+//        String[] str = {"Apple", "Banana", "Mango"};
+//        List<String> list2 = Arrays.asList(str);   // We cannot add new elements in .asList()
+//
+//        List<String> list3 = new ArrayList<>(list2);
+//        list3.add("Cherry");
+//        list3.addAll(list1);
+//        System.out.println(list3);
     }
 }
